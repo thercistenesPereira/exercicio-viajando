26fb74a63ccf8dcbe2d1bda19ba566c6bd2bf237
thercistenes <thercistenes1@outlook.com>
2024-07-09 17:08:18 -0300

    3 - Crie o componente <CardList />

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import './App.css';
 import Header from './components/Header';
-import Card from './components/Card';
+import CardList from './components/CardList';
 
 function App() {
   return (
     <main>
       <Header />
-      <Card />
+      <CardList />
     </main>
   );
 }
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,20 +2,20 @@ type CityInfoProps = {
   cityInfo: {
     city: string,
     country: string,
-    imageUr: string,
+    imageUrl: string,
     visited?: boolean,
   }
 };
 
 function Card({ cityInfo }: CityInfoProps) {
-  const { city, country, imageUr, visited = false } = cityInfo;
+  const { city, country, imageUrl, visited = false } = cityInfo;
   return (
-    <div>
+    <div className="card">
       <h2>{ country }</h2>
       <h3>{ city }</h3>
-      <img src={ imageUr } alt={ city } />
+      <img src={ imageUrl } alt={ city } />
       {
-        visited ? 'Já Fui!' : 'Não Fui.'
+        visited ? <p>Já Fui!</p> : <p>Não Fui.</p>
       }
     </div>
   );
